98bac9ae682632fdad69c1e8d0a0912df7d7ec5f
0LYL0 <1171368443@qq.com>
2017-07-14 09:56:34 +0800

    Initial Commit

---
+++ RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  你好
+//
+//  Created by 刘雅兰 on 2017/2/27.
+//  Copyright © 2017年 刘雅兰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
---
+++ 你好/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  你好
+//
+//  Created by 刘雅兰 on 2017/2/21.
+//  Copyright © 2017年 刘雅兰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
